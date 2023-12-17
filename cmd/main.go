7fb57f26143250c00f9be8e7df9453a88ce0f8fe
Khushi Sinha <khushisinha4786@gmail.com>
2023-12-17 19:24:36 +0530

    Code refactor

--- cmd/main.go
+++ cmd/main.go
@@ -11,104 +11,102 @@ type InventoryItem struct {
 	Quantity int    `json:"quantity"`
 }
 
+type RequestPayload struct {
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
+
 func main() {
 	app := gofr.New()
-	
-	type RequestPayload struct {
-		Name     string `json:"name"`
-		Quantity int    `json:"quantity"`
-	}
 
-	app.POST("/addInventory", func(ctx *gofr.Context) (interface{}, error) {
-		var requestPayload RequestPayload
-		if err := json.NewDecoder(ctx.Request().Body).Decode(&requestPayload); err != nil {
-			return nil, err
-		}
+	app.POST("/addInventory", addInventoryHandler)
 
-		// Insert data into the 'inventory' table
-		result, err := ctx.DB().ExecContext(ctx, "INSERT INTO inventory (name, quantity) VALUES (?, ?)", requestPayload.Name, requestPayload.Quantity)
-		if err != nil {
-			return nil, err
-		}
+	app.GET("/allInventory", allInventoryHandler)
 
-		// Get the ID of the newly inserted item
-		newID, err := result.LastInsertId()
-		if err != nil {
-			return nil, err
-		}
+	app.GET("/getInventory/{id}", getInventoryHandler)
 
-		return InventoryItem{ID: int(newID), Name: requestPayload.Name, Quantity: requestPayload.Quantity}, nil
-	})
+	app.PUT("/updateInventory/{id}", updateInventoryHandler)
 
-	// Read all inventory items
-	app.GET("/allInventory", func(ctx *gofr.Context) (interface{}, error) {
-		var inventoryItems []InventoryItem
-
-		rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM inventory")
-		if err != nil {
-			return nil, err
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			var item InventoryItem
-			if err := rows.Scan(&item.ID, &item.Name, &item.Quantity); err != nil {
-				return nil, err
-			}
-			inventoryItems = append(inventoryItems, item)
-		}
-
-		return inventoryItems, nil
-	})
-
-	// Read a specific inventory item by ID
-	app.GET("/getInventory/{id}", func(ctx *gofr.Context) (interface{}, error) {
-		// Extract inventory item ID from the URL parameters
-		id := ctx.PathParam("id")
-
-		// Query the database to get the specific inventory item
-		row := ctx.DB().QueryRowContext(ctx, "SELECT * FROM inventory WHERE id=?", id)
-
-		var item InventoryItem
-		if err := row.Scan(&item.ID, &item.Name, &item.Quantity); err != nil {
-			return nil, err
-		}
-
-		return item, nil
-	})
-
-	// Update an inventory item by ID
-	app.PUT("/updateInventory/{id}", func(ctx *gofr.Context) (interface{}, error) {
-		var requestPayload RequestPayload
-		if err := json.NewDecoder(ctx.Request().Body).Decode(&requestPayload); err != nil {
-			return nil, err
-		}
-
-		// Get the inventory item ID from the path parameters
-		itemID := ctx.Param("id")
-
-		// Update data in the 'inventory' table
-		_, err := ctx.DB().ExecContext(ctx, "UPDATE inventory SET name = ?, quantity = ? WHERE id = ?", requestPayload.Name, requestPayload.Quantity, itemID)
-		if err != nil {
-			return nil, err
-		}
-
-		return "Inventory item updated successfully", nil
-	})
-
-	// Delete an inventory item by ID
-	app.DELETE("/deleteInventory/{id}", func(ctx *gofr.Context) (interface{}, error) {
-		// Extract inventory item ID from the URL parameters
-		id := ctx.PathParam("id")
-
-		// Execute the DELETE query
-		_, err := ctx.DB().ExecContext(ctx, "DELETE FROM inventory WHERE id=?", id)
-		if err != nil {
-			return nil, err
-		}
-
-		return "Inventory item deleted successfully", nil
-	})
+	app.DELETE("/deleteInventory/{id}", deleteInventoryHandler)
 
 	app.Start()
+}
+
+func addInventoryHandler(ctx *gofr.Context) (interface{}, error) {
+	var requestPayload RequestPayload
+	if err := json.NewDecoder(ctx.Request().Body).Decode(&requestPayload); err != nil {
+		return nil, err
+	}
+
+	result, err := ctx.DB().ExecContext(ctx, "INSERT INTO inventory (name, quantity) VALUES (?, ?)", requestPayload.Name, requestPayload.Quantity)
+	if err != nil {
+		return nil, err
+	}
+
+	newID, err := result.LastInsertId()
+	if err != nil {
+		return nil, err
+	}
+
+	return InventoryItem{ID: int(newID), Name: requestPayload.Name, Quantity: requestPayload.Quantity}, nil
+}
+
+func allInventoryHandler(ctx *gofr.Context) (interface{}, error) {
+	var inventoryItems []InventoryItem
+
+	rows, err := ctx.DB().QueryContext(ctx, "SELECT * FROM inventory")
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		var item InventoryItem
+		if err := rows.Scan(&item.ID, &item.Name, &item.Quantity); err != nil {
+			return nil, err
+		}
+		inventoryItems = append(inventoryItems, item)
+	}
+
+	return inventoryItems, nil
+}
+
+func getInventoryHandler(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	row := ctx.DB().QueryRowContext(ctx, "SELECT * FROM inventory WHERE id=?", id)
+
+	var item InventoryItem
+	if err := row.Scan(&item.ID, &item.Name, &item.Quantity); err != nil {
+		return nil, err
+	}
+
+	return item, nil
+}
+
+func updateInventoryHandler(ctx *gofr.Context) (interface{}, error) {
+	var requestPayload RequestPayload
+	if err := json.NewDecoder(ctx.Request().Body).Decode(&requestPayload); err != nil {
+		return nil, err
+	}
+
+	itemID := ctx.Param("id")
+
+	_, err := ctx.DB().ExecContext(ctx, "UPDATE inventory SET name = ?, quantity = ? WHERE id = ?", requestPayload.Name, requestPayload.Quantity, itemID)
+	if err != nil {
+		return nil, err
+	}
+
+	return "Inventory item updated successfully", nil
+}
+
+func deleteInventoryHandler(ctx *gofr.Context) (interface{}, error) {
+	id := ctx.PathParam("id")
+
+	_, err := ctx.DB().ExecContext(ctx, "DELETE FROM inventory WHERE id=?", id)
+	if err != nil {
+		return nil, err
+	}
+
+	return "Inventory item deleted successfully", nil
 }

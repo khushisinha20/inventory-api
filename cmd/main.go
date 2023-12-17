9bc18d65e8bab1cb82f88acb19bfb2562e0e15c2
Khushi Sinha <khushisinha4786@gmail.com>
2023-12-17 13:25:16 +0530

    Add Inventory route

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,44 @@
 package main
 
-import "gofr.dev/pkg/gofr"
+import (
+	"encoding/json"
+	"gofr.dev/pkg/gofr"
+)
+
+type InventoryItem struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Quantity int    `json:"quantity"`
+}
 
 func main() {
-    app := gofr.New()
+	app := gofr.New()
+	
+	type RequestPayload struct {
+		Name     string `json:"name"`
+		Quantity int    `json:"quantity"`
+	}
 
-    app.GET("/greet", func(ctx *gofr.Context) (interface{}, error) {
+	app.POST("/addInventory", func(ctx *gofr.Context) (interface{}, error) {
+		var requestPayload RequestPayload
+		if err := json.NewDecoder(ctx.Request().Body).Decode(&requestPayload); err != nil {
+			return nil, err
+		}
 
-        return "Hello World!", nil
-    })
+		// Insert data into the 'inventory' table
+		result, err := ctx.DB().ExecContext(ctx, "INSERT INTO inventory (name, quantity) VALUES (?, ?)", requestPayload.Name, requestPayload.Quantity)
+		if err != nil {
+			return nil, err
+		}
 
-    app.Start()
+		// Get the ID of the newly inserted item
+		newID, err := result.LastInsertId()
+		if err != nil {
+			return nil, err
+		}
+
+		return InventoryItem{ID: int(newID), Name: requestPayload.Name, Quantity: requestPayload.Quantity}, nil
+	})
+
+	app.Start()
 }
